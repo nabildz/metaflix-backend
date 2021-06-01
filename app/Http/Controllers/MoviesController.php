eb902c7e98775c13baa3e845790473b029450dd1
nabildz <nabilkechroud@gmail.com>
2021-06-01 16:38:58 +0200

    Added TMDB API and movies index route

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -13,6 +13,9 @@ class MoviesController extends Controller
     public function index(){
 
 
+        return Http::get('https://api.themoviedb.org/3/movie/popular?api_key=d37692672a76f1c72bfdfcdd29a32768')
+        ->json()['results'];
+
     }
 
 }
