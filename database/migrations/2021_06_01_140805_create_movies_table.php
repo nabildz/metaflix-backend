5bc910e8bb6bfd6ec618813573c0dedc20bb1984
nabildz <nabilkechroud@gmail.com>
2021-06-01 16:22:17 +0200

    Added movies and genres table migrations

---
+++ database/migrations/2021_06_01_140805_create_movies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('tmbd_id');
+            $table->tinyInteger('tmbd_vote_average');
+            $table->string('language');
+            $table->string('title');
+            $table->string('image_url');
+            $table->date('release_date');
+            $table->enum('status', ['is_watched', 'in_wishlist']);
+            $table->enum('rating', ['yay', 'nay'])->nullable();
+            $table->string('review')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
