e41e887098fe26b4cf2f44a766571c5970046b70
nabildz <nabilkechroud@gmail.com>
2021-06-01 16:36:25 +0200

    Added Movies Contoller

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+use App\Models\Movie;
+
+class MoviesController extends Controller
+{
+
+    public function index(){
+
+
+    }
+
+}
